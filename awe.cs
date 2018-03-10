356708c3874988372c0a2399915c08a2f96d969d
HenriqueSamii <37251592+HenriqueSamii@users.noreply.github.com>
2018-03-10 18:26:30 -0300

    Create awe.cs

---
+++ awe.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int birthdayCakeCandles(int n, int[] ar) {
+        // Complete this function
+        var x = ar.;
+        return ar[x-1]
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] ar_temp = Console.ReadLine().Split(' ');
+        int[] ar = Array.ConvertAll(ar_temp,Int32.Parse);
+        int result = birthdayCakeCandles(n, ar);
+        Console.WriteLine(result);
+    }
+}
